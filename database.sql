894767ecc6a102a064169534374b2fc897ba5c34
DeividAlmeida <mr.davialmeida@hotmail.com>
2020-10-09 18:33:19 -0300

    in set

--- database.sql
+++ database.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS `ecommerce_paypal` (
   `link_cancelado` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `ecommerce_paypal` (`id`, `usuario`, `senha`, `token`, `moeda`, `link_retorno`, `link_cancelado`) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `ecommerce_paypal` (`id`, `usuario`, `senha`, `token`, `moeda`, `link_retorno`, `link_cancelado`) VALUES (1, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `ecommerce_plugins` (`id`, `titulo`, `nome`, `tipo`, `path`, `img`, `status`) VALUES (1, 'PayPal', 'paypal', 'gateways', 'ecommerce/plugins/gateways/paypal', 'ecommerce/plugins/gateways/paypal/wa/assets/img/paypal.jpg', '');
